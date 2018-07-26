51ba9e8ebe2d0958aefb3ebcec0ce75215fc7bed
Michał Kurkowski <michalkurkowski93@gmail.com>
2018-07-26 23:04:53 +0200

    default folder

--- emojiToPNG/main.swift
+++ emojiToPNG/main.swift
@@ -6,7 +6,22 @@
 //  Copyright © 2018 Michał Kurkowski. All rights reserved.
 //
 
-import Foundation
+import AppKit
 
-print("Hello, World!")
+class EmojiAsImage {
+  
+  func dirPath() -> String {
+    let pathFromArgs: String? = CommandLine.argc > 1 ? CommandLine.arguments[1] : nil
+    var isDir  = false as ObjCBool
+    let flMngr = FileManager.default
+    if pathFromArgs != nil && flMngr.fileExists(atPath: pathFromArgs!, isDirectory: &isDir) {
+      return pathFromArgs!
+    } else {
+      return flMngr.urls(for: .desktopDirectory, in: .userDomainMask).first!.path
+    }
+  }
+}
+
+let emoji = EmojiAsImage()
+print(emoji.dirPath())
 
