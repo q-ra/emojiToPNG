3728d97cf74c76f5c0d1e3c1995b229dc9094c1c
Michał Kurkowski <michalkurkowski93@gmail.com>
2018-07-26 21:49:54 +0200

    Initial Commit

---
+++ emojiToPNG/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  emojiToPNG
+//
+//  Created by Michał Kurkowski on 26/07/2018.
+//  Copyright © 2018 Michał Kurkowski. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
